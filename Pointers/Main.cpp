5d5d563fc56b7a416bf4feda7e0a8c885166f1ca
ДОМ <@DESKTOP-S2HEBGI>
2022-10-15 13:02:05 +0300

    Add project files.

---
+++ Pointers/Main.cpp
@@ -0,0 +1,26 @@
+//Pointers
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	/*int a = 2;
+	int* pa = &a;
+	cout << a << endl; //вывод значения переменной
+	cout << &a << endl; //взятие адреса переменной а прямо при выводе
+	cout << pa << endl; // выод адреса переменной а, записанный в указатель pa
+	cout << *pa << endl;//разименовываем указатель pa и получаем значение,
+	                    // которое хранится по адресу*/
+
+	const int n = 5;
+	int arr[n] = { 3,5,8,13,21 };
+	cout << arr << endl;
+	cout << *arr << endl;
+	for (int i = 0; i < n;i++)
+	{
+		cout << *(arr + i) << "\t";
+	}
+
+
+}
