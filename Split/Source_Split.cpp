005f45286b1d2f35e7a1abcac12d03fce4e6b43b
ДОМ <@DESKTOP-S2HEBGI>
2022-10-25 23:30:54 +0300

    split_hausaufgabe

--- Split/Source_Split.cpp
+++ Split/Source_Split.cpp
@@ -6,7 +6,7 @@ template <typename T> void FillRand(T arr[], const int n);
 template <typename T> void Print (T arr[], const int n);
 template <typename T> int EVEN_COUNT(T arr[], const int n);
 template <typename T> int ODD_COUNT(T arr[], const int n);
-//template <typename T> void FillRand_EVEN_ODD(T arr[], const int n, T brr[], int m, T crr[], int f);
+
 
 void main()
 	{
@@ -22,7 +22,7 @@ void main()
 	int* even = new int[n_even];
 	int* odd = new int[n_odd];
 
-	//FillRand_EVEN_ODD(arr, n, even, n_even, odd, n_odd);// почему-то через функцию не работает: пишет, что even и odd identifier not found
+
 	int j = 0, k = 0;
 	for (int i = 0; i < n; i++)
 	{
@@ -83,22 +83,6 @@ template <typename T> int ODD_COUNT(T arr[], const int n)
 	return count;
 }
 
-/*template <typename T> void FillRand_EVEN_ODD(T arr[], const int n, T brr[], int m, T crr[], int f)
-{
-	int j = 0, k=0;
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i] % 2 == 0)
-		{
-			even[j]=*(arr+i);
-			j++;
-		}
-		else 
-		{
-			odd[k] = arr[i];
-			k++;
-		}
-	}
-}*/
+
 
 
